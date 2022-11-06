fd37357a720e3c214a600cb6ff70cc5bd8d87c48
hanjokim <hanjokim@users.noreply.github.com>
2022-11-06 18:17:24 +0900

    trivial changes

--- gps_parse_pymnea2.py
+++ gps_parse_pymnea2.py
@@ -7,7 +7,8 @@ import pynmea2
 import serial
 
 
-ser = serial.Serial('/dev/ttyAMA2', 9600, timeout=0.5)                      # RPi GPIO
+# ser = serial.Serial('/dev/ttyAMA2', 9600, timeout=1)                      # RPi GPIO
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)                      # RPi U-blox7
 # ser = serial.Serial('/dev/cu.usbserial-A50285BI', 9600, timeout=1)      # Mac USB-FTDI
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 
@@ -15,8 +16,8 @@ while 1:
     try:
         line = sio.readline()
         msg = pynmea2.parse(line)
-        print(msg)
         if 'RMC' in msg.identifier():
+            print(msg)
             print(msg.status, msg.is_valid, msg.latitude, msg.lat_dir, msg.longitude, msg.lon_dir)
             # dt = msg.datetime - timedelta(hours=-9)
             # print(dt)
