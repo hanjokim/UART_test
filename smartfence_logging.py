12653081ade4413118a1d0560e1c168d09cc3311
Hanjo Kim <kimhanjo@hotmail.com>
2023-06-12 19:53:18 +0900

    added smartfence logging module and .ini

--- smartfence_logging.py
+++ smartfence_logging.py
@@ -1,4 +1,34 @@
 # -*- coding: utf-8 -*-
+'''
+1. 수집 날짜 및 시간정보 (ex. 2023/03/20 17:09:43) (10초 간격으로 데이터 수집)
+2. PM1.0 (ex. 27)
+3. PM2.5 (ex. 39)
+4. PM10 (ex. 44)
+5. 온도 (ex. 19.3)
+6. 습도 (ex. 15.3)
+7. PM 1.0 1분간 average (ex. 25, 35, 45)
+8. PM 1.0 1분간 min (ex. 22, 31, 41)
+9. PM 1.0 1분간 max (ex. 28, 39, 48)
+10. PM 1.0 1분간 median (ex. 25, 34, 44)
+11. PM 1.0 1분간 trimmed mean (ex. 25, 35, 45)
+12. PM 2.5 1분간 average (ex. 25, 35, 45)
+13. PM 2.5 1분간 min (ex. 22, 31, 41)
+14. PM 2.5 1분간 max (ex. 28, 39, 48)
+15. PM 2.5 1분간 median (ex. 25, 34, 44)
+16. PM 2.5 1분간 trimmed mean (ex. 25, 35, 45)
+17. PM 10 1분간 average (ex. 25, 35, 45)
+18. PM 10 1분간 min (ex. 22, 31, 41)
+19. PM 10 1분간 max (ex. 28, 39, 48)
+20. PM 10 1분간 median (ex. 25, 34, 44)
+21. PM 10 1분간 trimmed mean (ex. 25, 35, 45)
+22. 위도
+23. 경도
+24. PM 센서 상태 "OK" / "NG"
+25. GPS 센서 상태 "FX" / "NF" Fixed / Not Fixed
+26. 현재 팬 동작 상태 (대기, 자동(1, 2, 3, 4, 5단), 수동(1, 2, 3, 4, 5단)) 'W' / 'A' / 'M'
+27. 장치 오류 알림 "OK" / "NG"
+28. Checksum
+'''
 import os
 
 # import requests
@@ -10,6 +40,7 @@ import serial
 import time
 import threading
 import struct
+import statistics
 from datetime import datetime
 from datetime import timedelta
 # import logging
@@ -24,7 +55,7 @@ from luma.oled.device import ssd1306,  sh1106
 from PIL import ImageFont
 
 conf_values = configparser.ConfigParser()
-conf_values.read('config.ini')
+conf_values.read('config_smartfence.ini')
 general = conf_values['GENERAL']
 pm = conf_values['PM']
 gps = conf_values['GPS']
@@ -49,30 +80,47 @@ update_interval = general.getfloat('update_interval')
 thread_interval = general.getfloat('thread_interval')
 log_mode = general['log_mode']
 log_interval = general.getint('log_interval')
-
-pm_err_count = 0
-gps_err_count = 0
+average_window = general.getint('average_window')
+sample_no = int(average_window / update_interval)
+trim_percent = general.getint('trim_percent')
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 # formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
 
+
 if log_mode == 'midnight':
     log_interval = 1
 
-timedfilehandler = logging.handlers.TimedRotatingFileHandler(filename='log/finedustlog', when=log_mode, interval=log_interval, encoding='utf-8', utc=False)
+timedfilehandler = logging.handlers.TimedRotatingFileHandler(filename='log/smartfencelog', when=log_mode, interval=log_interval, encoding='utf-8', utc=False)
 # timedfilehandler.setFormatter(formatter)
-timedfilehandler.suffix = "%Y%m%d_%H%M%S"
+timedfilehandler.suffix = "%Y%m%d"
+# timedfilehandler.suffix = "%Y%m%d_%H%M%S"
 
 logger.addHandler(timedfilehandler)
 meas_data = {
-    "pm1":  None,
-    "pm25": None,
-    "pm10": None,
+    "pm1": 0,
+    "pm25": 0,
+    "pm10": 0,
     "temp": None,
     "humi": None,
     "long": None,
     "lati": None,
+    "pm1_average": 0,
+    "pm25_average": 0,
+    "pm10_average": 0,
+    "pm1_min": 0,
+    "pm25_min": 0,
+    "pm10_min": 0,
+    "pm1_max": 0,
+    "pm25_max": 0,
+    "pm10_max": 0,
+    "pm1_median": 0,
+    "pm25_median": 0,
+    "pm10_median": 0,
+    "pm1_tmean": 0,
+    "pm25_tmean": 0,
+    "pm10_tmean": 0,
     "timestamp": "",
 }
 
@@ -88,7 +136,7 @@ try:
     elif oled_driver == 'sh1106':
         device = sh1106(iface, rotate=disp_rotate)
     else:
-        print("OLED driver configuration error in config.ini")
+        print("OLED driver configuration error in config_smartfence.ini")
         sys.exit()
 except Exception as e:
     print("Display device initialization error:", e)
@@ -117,10 +165,36 @@ def disp_OLED(meas_data):
         draw.text((x, top + 50), "Lati: %-10s" % str(meas_data["lati"] if meas_data["lati"] else "-"), font=font, fill=255)
 
 
+def get_fan_status():
+    fan_status = "W"
+    return fan_status
+
+
+def get_device_status():
+    device_status = "OK"
+    return device_status
+
+
+def get_checksum(data):
+    return 1
+
+def calculate_stats(data, trim_percent):
+    mean = statistics.mean(data)
+    minimum = min(data)
+    maximum = max(data)
+    median = statistics.median(data)
+
+    trim_size = int(len(data) * trim_percent / 100)
+    trimmed_data = sorted(data)[trim_size:-trim_size]
+    trimmed_mean = statistics.mean(trimmed_data)
+
+    return mean, minimum, maximum, median, trimmed_mean
+
+
 # 데이터 처리할 함수
 def parsing_pm_data(packed_data):
     tmp = struct.unpack('!16h', packed_data)
-    print(f'PM input data : {tmp}')
+    # print(f'PM input data : {tmp}')
     if check_pm_data(tmp) == 1:
         return tmp
     else:
@@ -147,7 +221,7 @@ def parsing_gps_data(gps_bytes):
         string = gps_bytes.decode('utf-8')
         gps_data = string.rstrip().split(',')
         if check_gps_data(gps_data) == 1:
-            print(f'GPS input data : {gps_data}')
+            # print(f'GPS input data : {gps_data}')
             return gps_data
         else:
             return -1
@@ -165,10 +239,14 @@ def check_gps_data(data):
 
 # 본 쓰레드
 def readThread(pm_ser, gps_ser):
-    global line
+    # global line
     global exitThread
     global clock_set
     global pm_err_count
+    # global pm_status
+    # global gps_status
+    global sample_no
+    global trim_percent
 
     # 쓰레드 종료될때까지 계속 돌림
     while not exitThread:
@@ -177,12 +255,19 @@ def readThread(pm_ser, gps_ser):
         if len(temp) == pm_data_size:
             pm_data = parsing_pm_data(temp)
             if pm_data != -1:
-                # continue
+                # pm_status = "OK"
                 meas_data["pm1"] = pm_data[2]
                 meas_data["pm25"] = pm_data[3]
                 meas_data["pm10"] = pm_data[4]
                 meas_data["temp"] = (pm_data[12] / 10.) if is_PMS7003T else None
                 meas_data["humi"] = (pm_data[13] / 10.) if is_PMS7003T else None
+            else:
+                # pm_status = "NG"
+                meas_data["pm1"] = 0
+                meas_data["pm25"] = 0
+                meas_data["pm10"] = 0
+                meas_data["temp"] = None
+                meas_data["humi"] = None
         else:
             pm_ser.reset_input_buffer()
 
@@ -199,6 +284,8 @@ def readThread(pm_ser, gps_ser):
                             clock_set = True
                 meas_data["long"] = float(gps_data[3]) if gps_data[3] else None
                 meas_data["lati"] = float(gps_data[5]) if gps_data[5] else None
+            meas_data["long"] = None
+            meas_data["lati"] = None
 
         else:
             gps_ser.flushInput()
@@ -219,55 +306,78 @@ if __name__ == "__main__":
     # 시작!
     thread.start()
 
-    pm_status = 0
-    gps_status = 0
+    fan_status = "W"
+    device_status = "NG"
+    pm_status = "NG"
+    gps_status = "NF"
+    checksum = 0
+
+    # list for average sampling data
+    stat_cycle = 0
+    sample_pm1 = []
+    sample_pm25 = []
+    sample_pm10 = []
+    stats_pm1 = []
+    stats_pm25 = []
+    stats_pm10 = []
 
     try:
         while True:
-            msg_status = "Not ready"
+            stat_cycle += 1
 
             if None in [meas_data["pm1"], meas_data["pm25"], meas_data["pm10"], meas_data["temp"], meas_data["humi"]]:
-                pm_status = 0
-                pm_err_count += 1
-                print(f"PM input error : {pm_err_count=}")
-                # if pm_err_count > 5:
-                #     pm_reset()
+                pm_status = "NG"
             else:
-                pm_status = 1
-                pm_err_count = 0
+                pm_status = "OK"
 
             if None in [meas_data["long"], meas_data["lati"]]:
-                gps_status = 0
+                gps_status = "NF"
             else:
-                gps_status = 1
+                gps_status = "FX"
 
             # dtstring = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
             dtstring = meas_data['timestamp']
 
-            if pm_status == 1 and (gps_status == 1 or clock_set is True):
-                print(f"{pm_status=}, {gps_status=}, {clock_set=}")
-                # res = sendData() --> loggindg
-                logger.info(f'{dtstring},{meas_data["pm1"]},{meas_data["pm25"]},{meas_data["pm10"]},'
-                            f'{meas_data["temp"]},{meas_data["humi"]},{meas_data["long"]},{meas_data["lati"]}')
-                # logger.info("%s,%.1f,%.1f,%.1f,%.1f,%.1f,%.5f,%.5f",
-                #             dtstring,
-                #             meas_data["pm1"], meas_data["pm25"], meas_data["pm10"], meas_data["temp"], meas_data["humi"],
-                #             meas_data["long"], meas_data["lati"])
-                print("Logged OK- %s" % meas_data)
-            else:
-                if pm_status == 0:
-                    msg_status += " PM"
-                if gps_status == 0:
-                    msg_status += " GPS"
-                print(msg_status, dtstring, '-', meas_data)
+            fan_status = get_fan_status()
+            device_status = get_device_status()
+
+            sample_pm1.append(meas_data["pm1"])
+            sample_pm25.append(meas_data["pm25"])
+            sample_pm10.append(meas_data["pm10"])
+
+            if stat_cycle == sample_no:
+                meas_data["pm1_average"], meas_data["pm1_min"], meas_data["pm1_max"], meas_data["pm1_median"], meas_data["pm1_tmean"] = calculate_stats(sample_pm1, trim_percent)
+                meas_data["pm25_average"], meas_data["pm25_min"], meas_data["pm25_max"], meas_data["pm25_median"], meas_data["pm25_tmean"] = calculate_stats(sample_pm25, trim_percent)
+                meas_data["pm10_average"], meas_data["pm10_min"], meas_data["pm10_max"], meas_data["pm10_median"], meas_data["pm10_tmean"] = calculate_stats(sample_pm10, trim_percent)
+                sample_pm1 = []
+                sample_pm25 = []
+                sample_pm10 = []
+                stat_cycle = 0
+
+            checksum = get_checksum(meas_data)
+            print(f'{meas_data=}')
+
+            logger.info(f'{dtstring},{meas_data["pm1"]},{meas_data["pm25"]},{meas_data["pm10"]},'
+                        f'{meas_data["pm1_average"]:.1f},{meas_data["pm25_average"]:.1f},{meas_data["pm10_average"]:.1f},'
+                        f'{meas_data["pm1_min"]},{meas_data["pm25_min"]},{meas_data["pm10_min"]},'
+                        f'{meas_data["pm1_max"]},{meas_data["pm25_max"]},{meas_data["pm10_max"]},'
+                        f'{meas_data["pm1_median"]},{meas_data["pm25_median"]},{meas_data["pm10_median"]},'
+                        f'{meas_data["pm1_tmean"]:.1f},{meas_data["pm25_tmean"]:.1f},{meas_data["pm10_tmean"]:.1f},'
+                        f'{meas_data["temp"]},{meas_data["humi"]},{meas_data["long"]},{meas_data["lati"]},'
+                        f'{pm_status},{gps_status},{fan_status},{device_status},{checksum}')
+            # logger.info("%s,%.1f,%.1f,%.1f,%.1f,%.1f,%.5f,%.5f",
+            #             dtstring,
+            #             meas_data["pm1"], meas_data["pm25"], meas_data["pm10"], meas_data["temp"], meas_data["humi"],
+            #             meas_data["long"], meas_data["lati"])
+            print("Logged OK- %s" % meas_data, f'{pm_status=},{gps_status=},{fan_status=},{device_status=},{checksum=}')
 
             # _dt = datetime.fromtimestamp(int(meas_data["timestamp"])).strftime('%Y-%m-%d %H:%M:%S')
 
             disp_OLED(meas_data)
 
-            meas_data["pm1"] =  None
-            meas_data["pm25"] = None
-            meas_data["pm10"] = None
+            meas_data["pm1"] =  0
+            meas_data["pm25"] = 0
+            meas_data["pm10"] = 0
             meas_data["temp"] = None
             meas_data["humi"] = None
             meas_data["long"] = None
